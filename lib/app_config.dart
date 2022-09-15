b9b4de43ceda4c7741e113d49ff4663ffb91331f
Salman Ahmad Bhatti <salmanbhatti52@hotmail.com>
2022-09-15 15:08:47 +0500

    Initial Code
    App New Code

---
+++ lib/app_config.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+var this_year = DateTime.now().year.toString();
+
+class AppConfig {
+  static String copyright_text =
+      "@ SAMIMIE " + this_year; //this shows in the splash screen
+  static String app_name =
+      "SAMIMIE eCommerce"; //this shows in the splash screen
+  static String purchase_code =
+      "0678906b-c654-4513-b9f9-48c55d365e15"; //enter your purchase code for the app from codecanyon
+
+  //Default language config
+  static String default_language = "en";
+  static String mobile_app_code = "en";
+  static bool app_language_rtl = false;
+
+  //configure this
+  static const bool HTTPS = true;
+
+  //configure this
+
+  // static const DOMAIN_PATH = "192.168.6.1/folder"; //localhost
+  //
+  static const DOMAIN_PATH = "samimie.com"; // directly inside the public folder
+
+  //do not configure these below
+  static const String API_ENDPATH = "api/v2";
+  static const String PROTOCOL = HTTPS ? "https://" : "http://";
+  static const String RAW_BASE_URL = "${PROTOCOL}${DOMAIN_PATH}";
+  static const String BASE_URL = "${RAW_BASE_URL}/${API_ENDPATH}";
+}
